9715d860f2254ccdce9cd4bfb0724a3a429fc8e9
Sunil112 <sunil.sonu112@gmail.com>
2026-08-15 14:02:40 +0530

    assetsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PartiesModule } from './parties/parties.module';
 import { RelationshipsModule } from './relationships/relationships.module';
+import { AssetsModule } from './assets/assets.module';
+import { MintModule } from './mint/mint.module';
+import { BurnModule } from './burn/burn.module';
+import { SwapModule } from './swap/swap.module';
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [PartiesModule, RelationshipsModule],
+  imports: [
+    PartiesModule,
+    RelationshipsModule,
+    AssetsModule,
+    MintModule,
+    BurnModule,
+    SwapModule,
+    CustomersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
